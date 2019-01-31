ef2d88533ac7811521bddea457a3058a999a6607
Tom <tom@myoxygen.co.uk>
2019-01-31 09:15:54 +0000

    First commit.
    First commit.

---
+++ SimpleXam.Fundamentals/SimpleXam.Fundamentals.iOS/CircleViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using SimpleXam.Fundamentals;
+using SimpleXam.Fundamentals.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
+namespace SimpleXam.Fundamentals.iOS
+{
+    public class CircleViewRenderer : BoxRenderer
+    {
+        private CircleView CustomElement => Element as CircleView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+            {
+                return;
+            }
+
+            Layer.MasksToBounds = true;
+            Layer.CornerRadius = (float)CustomElement.Radius / 2.0f;
+        }
+    }
+}
---
+++ SimpleXam.Fundamentals/SimpleXam.Fundamentals.iOS/StaticLabelRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using SimpleXam.Fundamentals;
+using SimpleXam.Fundamentals.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(StaticLabel), typeof(StaticLabelRenderer))]
+namespace SimpleXam.Fundamentals.iOS
+{
+    public class StaticLabelRenderer : LabelRenderer
+    {
+        public StaticLabelRenderer() : base()
+        {
+        }
+    }
+}
